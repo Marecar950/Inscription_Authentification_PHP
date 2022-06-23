d3016243ab11a055e26862c27598ff2863edd457
Mareacr950 <mouzammilm2000@gmail.com>
2022-06-23 11:30:40 +0200

    Connexion à la base de données

---
+++ connexion_base.php
@@ -0,0 +1,14 @@
+<?php
+
+$host_name = "localhost";
+$database = "users";
+$username = "marecar";
+$password = "mdpasse_marec";
+
+try {
+  $pdo=new PDO('mysql:host='.$host_name.';dbname='.$database,$username,$password);
+  }
+  catch (PDOException $e) {
+  echo "Error! : ".$e->getMessage();
+  }
+?>
