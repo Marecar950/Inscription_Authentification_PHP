58fa31d9c420eadcfb2447729331f4d7aa92e94d
Marecar950 <mouzammilm2000@gmail.com>
2022-08-10 17:17:35 +0200

    Connexion à la base de données

--- connexion_base.php
+++ connexion_base.php
@@ -1,9 +1,9 @@
 <?php
 
-$host_name = "localhost";
-$database = "users";
-$username = "marecar";
-$password = "mdpasse_marec";
+$host_name = "sql210.epizy.com";
+$database = "epiz_32347783_users";
+$username = "epiz_32347783";
+$password = "5Lx6hxRtT4LGY";
 
 try {
   $pdo=new PDO('mysql:host='.$host_name.';dbname='.$database,$username,$password);
